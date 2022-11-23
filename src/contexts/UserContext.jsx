8e4368f4072710c59eff8d661d39313d11351406
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2022-11-24 03:14:56 +0800

    context update

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -4,11 +4,21 @@ export let UserContext = createContext({});
 
 export const UserContextProvider = ({ children }) => {
 
-  const [loginInfo, setLoginInfo] = useState({})
-  const [loginInfoHeader, setLoginInfoHeader] = useState({})
+  const [loginInfo, setLoginInfo] = useState(sessionStorage.getItem("loginInfo") === null ? {} : JSON.parse(sessionStorage.getItem("loginInfo")));
+  const [loginInfoHeader, setLoginInfoHeader] = useState(sessionStorage.getItem("loginHeaders") === null ? {} : JSON.parse(sessionStorage.getItem("loginHeaders")));
+
+  const updateLoginInfo = (info) => {
+    sessionStorage.setItem('loginInfo', JSON.stringify(info))
+    setLoginInfo(info)
+  }
+
+  const updateLoginInfoHeader = (info) => {
+    sessionStorage.setItem('loginInfo', JSON.stringify(info))
+    setLoginInfoHeader(info)
+  }
 
   return (
-    <UserContext.Provider value={{ loginInfo, setLoginInfo, loginInfoHeader, setLoginInfoHeader}}>
+    <UserContext.Provider value={{ loginInfo, setLoginInfo, loginInfoHeader, setLoginInfoHeader, updateLoginInfo, updateLoginInfoHeader}}>
       {children}
     </UserContext.Provider>
   )
